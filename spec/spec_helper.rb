ae1e82608d0ac053011674d5e23fbef282fbc97b
Marcos Mercedes <marcos.mercedesn@gmail.com>
2020-04-16 22:47:41 -0400

    Add filtered run for rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,7 @@ require "dotenv/load"
 require "bundler/setup"
 require "webmock/rspec"
 require "gfr_image_transformer"
+require "awesome_print"
 
 WebMock.disable_net_connect!(allow_localhost: true)
 
@@ -13,6 +14,9 @@ RSpec.configure do |config|
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
